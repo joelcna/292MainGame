d34a2f6390fd95cbb33d86753dd33d4cf8d329c3
joelcna <joelcna28@gmail.com>
2023-11-07 16:46:55 -0500

    11/7/23 4:46

---
+++ MainGame292Td/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves;
+    [SerializeField] private float difficultyScaling = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeft;
+    private bool isSpawning = true;
+
+    private void Start()
+    {
+        StartWave();
+    }
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawning) return;
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeft > 0)
+        {
+            SpawnEnemy();
+            enemiesLeft--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
+        }
+    }
+    private void StartWave()
+    {
+        enemiesLeft = enemiesPerWave();
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject prefabToSpawn = enemyPrefabs[0];
+        GameObject enemy = Instantiate(prefabToSpawn, LevelManager.main.StartPoint.position, Quaternion.identity);
+
+        Rigidbody2D enemyRigidbody = enemy.GetComponent<Rigidbody2D>();
+        if (enemyRigidbody != null)
+        {
+            enemyRigidbody.velocity = Vector2.zero;
+        }
+    }
+    private int enemiesPerWave()
+    {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, 0.75f));
+    }
+}
